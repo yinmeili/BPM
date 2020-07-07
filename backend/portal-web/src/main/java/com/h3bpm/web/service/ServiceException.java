e9e5221d767895438884da070dde3e769bb61e28
tggyyh <993792497@qq.com>
2020-07-07 10:12:29 +0800

    加入 ResponseVo ，serviceException

---
+++ backend/portal-web/src/main/java/com/h3bpm/web/service/ServiceException.java
@@ -0,0 +1,43 @@
+package com.chinacloud.reportservice;
+
+/**
+ * This exception will be thrown when some errors occur in the platform layer.
+ * 
+ * @author Tonghao
+ */
+public class ServiceException extends Exception {
+	private static final long serialVersionUID = 2390553120295672021L;
+
+	private int errorCode = 0;
+
+	public ServiceException(int errorCode, String message, Throwable cause) {
+		super(message, cause);
+
+		this.errorCode = errorCode;
+	}
+
+	public ServiceException(int errorCode, String message) {
+		this(errorCode, message, null);
+	}
+
+	public ServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ServiceException(String message) {
+		this(message, null);
+	}
+
+	public ServiceException(Exception ex) {
+		this(ex.getMessage(), ex.getCause());
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	@Override
+	public String toString() {
+		return "[errorCode=" + getErrorCode() + ",message=" + getMessage() + "]";
+	}
+}
