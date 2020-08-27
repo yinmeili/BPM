b8ee9cad38cdee33c53f0b84ed97c4d3da3c1ead
tggyyh <993792497@qq.com>
2020-08-27 09:09:53 +0800

    后端 WorkflowTask加入日志输出

--- backend/portal-web/src/main/java/com/h3bpm/web/service/WorkFlowService.java
+++ backend/portal-web/src/main/java/com/h3bpm/web/service/WorkFlowService.java
@@ -1,18 +1,17 @@
 package com.h3bpm.web.service;
 
-import OThinker.H3.Controller.RestfulApi.Entity.RestfulApiResult;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.h3bpm.web.entity.WorkFlowTask;
 import com.h3bpm.web.enumeration.ApiActionUrl;
 import com.h3bpm.web.enumeration.WorkflowExecuteType;
 import com.h3bpm.web.mapper.WorkFlowTaskMapper;
-import org.apache.poi.ss.formula.functions.T;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.lang.annotation.Target;
-import java.util.*;
 
 @Service
 public class WorkFlowService extends ApiDataService{
@@ -24,7 +23,8 @@ public class WorkFlowService extends ApiDataService{
      * 新建一个流程
      * @param id
      */
-    public String createWorkFlow(String id){
+    @SuppressWarnings("unchecked")
+	public String createWorkFlow(String id){
         WorkFlowTask workFlowTask = null;
 
         try{
@@ -35,7 +35,7 @@ public class WorkFlowService extends ApiDataService{
             map.put("finishStart",false);
             map.put("workflowCode",workFlowTask.getWorkFlowCode());
 
-            Map tmp = this.processSyncBpm(String.format(ApiActionUrl.CREATE_WORKFLOW.getUrl(),workFlowTask.getWorkFlowCode()), ApiActionUrl.CREATE_WORKFLOW.getHttpRequestType(), map);
+            Map<String, Object> tmp = this.processSyncBpm(String.format(ApiActionUrl.CREATE_WORKFLOW.getUrl(),workFlowTask.getWorkFlowCode()), ApiActionUrl.CREATE_WORKFLOW.getHttpRequestType(), map);
 
             if(tmp != null){
                 Map<String,Object> data = (Map<String,Object>)tmp.get("data");
