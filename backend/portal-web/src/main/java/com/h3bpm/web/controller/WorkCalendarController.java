2410723ef584f9ada8b6422d2ae3340148956ef0
lzf <51181213076@stu.ecnu.edu.cn>
2020-09-01 13:38:21 +0800

    修改查询工作日历流程WorkCalendarController的响应数据格式

--- backend/portal-web/src/main/java/com/h3bpm/web/controller/WorkCalendarController.java
+++ backend/portal-web/src/main/java/com/h3bpm/web/controller/WorkCalendarController.java
@@ -35,7 +35,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.security.sasl.AuthenticationException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
