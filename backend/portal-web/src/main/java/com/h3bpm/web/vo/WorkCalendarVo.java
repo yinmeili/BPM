1b7d3961cda978aa46e47d59cee5682af0be356f
tggyyh <993792497@qq.com>
2020-10-13 09:03:20 +0800

    后端 工作日历返回任务时间 修改为 Long 类型

--- backend/portal-web/src/main/java/com/h3bpm/web/vo/WorkCalendarVo.java
+++ backend/portal-web/src/main/java/com/h3bpm/web/vo/WorkCalendarVo.java
@@ -1,15 +1,11 @@
 package com.h3bpm.web.vo;
 
-import java.util.Date;
-
-import com.h3bpm.web.entity.File;
-
 public class WorkCalendarVo {
 	private String id = null;	//ObjectID流程ID
 	private String title = null;//InstanceName流程名称
 	private int status = 0;		//流程状态
-	private Date start = null;
-	private Date end = null;
+	private Long start = null;
+	private Long end = null;
 
 	public String getId() {
 		return id;
@@ -35,19 +31,19 @@ public class WorkCalendarVo {
 		this.status = status;
 	}
 
-	public Date getStart() {
+	public Long getStart() {
 		return start;
 	}
 
-	public void setStart(Date start) {
+	public void setStart(Long start) {
 		this.start = start;
 	}
 
-	public Date getEnd() {
+	public Long getEnd() {
 		return end;
 	}
 
-	public void setEnd(Date end) {
+	public void setEnd(Long end) {
 		this.end = end;
 	}
 }
