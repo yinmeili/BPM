6ed8c46155322bc294923a5ddef8a8c35bdf0722
CaiYuJie-WEB <231059354@qq.com>
2020-12-08 09:20:59 +0800

    前端修改蔡宇杰12月8号

--- frontend/Portal/workflowTask/modals.js
+++ frontend/Portal/workflowTask/modals.js
@@ -1,9 +1,41 @@
 app.controller("workflowTaskController", ["$scope", '$q', "$rootScope", "$http", "$translate", "$state", "$filter", "$modal", "$modalInstance", "$interval", "$timeout", "ControllerConfig", "notify", "datecalculation", "params",
     function ($scope, $q, $rootScope, $http, $translate, $state, $filter, $modal, $modalInstance, $interval, $timeout, ControllerConfig, notify, datecalculation, params) {
         $scope.params = params;
+      
+        $scope.workFlow = [
+            { workFlowname: '清算', id: "liquidation" },
+        ];
         params.AgencyID;
-        $scope.workFlow = params.AgencyID;
 
+        $scope.init = function () {
+            if (params.AgencyID!=undefined) {
+            if (params.AgencyID!="") {
+                $scope.editOperatorWorkFlow = params.AgencyID.workflowCodeStr;
+                $scope.editOperatorName =params.AgencyID.userDisplayName;
+                var time = new Date(params.AgencyID.startTime);
+                
+                var year = time.getFullYear();
+                var month = time.getMonth() + 1
+                var date = time.getDate() < 10 ? "0" + time.getDate() : time.getDate();
+                var hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+                var minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+                var seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+                params.AgencyID.startTime = year + '-' + (month) + '-' + (date) + ' ' + (hours) + ':' + (minutes) + ':' + (seconds);
+                $scope.editOperatorCreateTime = params.AgencyID.startTime;
+                if (params.AgencyID.executeType == 'null' || params.AgencyID.executeType == null) {
+                    params.AgencyID.executeType = '';
+                }
+               else if(params.AgencyID.executeType==1){
+                params.AgencyID.executeType="是"
+               }
+               else if(params.AgencyID.executeType==0){
+                params.AgencyID.executeType="否"
+               }
+                $scope.editOperatorIfCreate = params.AgencyID.executeType;
+            }
+            }
+        }
+        $scope.init();
 
         deferredHandler = function (data, deferred, defaultMsg) {
             if (!data || typeof data !== 'object') {
@@ -120,6 +152,50 @@ app.controller("workflowTaskController", ["$scope", '$q', "$rootScope", "$http",
                
         }
 
+
+        editWorkFlow = function () {
+
+           
+            var deferred = $q.defer();
+            var data = {
+                id:params.AgencyID.id,
+                userDisplayName: $scope.editOperatorName,
+            };
+
+            $http.post('/Portal/workflowTask/updateWorkFlowTaskUser', data).success(function (data) {
+             deferredHandler(data, deferred); 
+              $.notify({ message: data.msg, status: "success" });
+              $scope.cancel();
+              
+            }).error(function (data) {
+                // deferredHandler(data, deferred, $translate.instant('error_creating_folder'));
+                $.notify({ message: data.msg, status: "danger" });
+            })['finally'](function () {
+
+            });
+            return deferred.promise;
+        }
+
+        //编辑
+        $scope.editOWorkflowOperator = function () {
+
+            var OperatorName = $scope.editOperatorName;
+            var msg = "";
+            if (!OperatorName) {
+                msg += '请输入执行人名字 | ';
+            }
+           
+            msg = msg.substr(0, msg.length - 3);
+            if (!msg) {
+                editWorkFlow().then(function () {
+                    $("#tabWorkFlowTask").dataTable().fnDraw();
+                });
+            } else {
+                $.notify({ message: msg, status: "danger" });
+                return false;
+            }
+            
+        };
 
 
     }]);
